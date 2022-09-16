7c624eaea776d85dd6c3ee156bb0bc5f09b43d43
flvSantos15 <flvSantos300@gmail.com>
2022-09-16 09:42:13 -0300

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+  [SerializeField] float runSpeed = 5f;
+  [SerializeField] float jumpSpeed = 5f;
+  [SerializeField] float climbSpeed = 5f;
+
+  Vector2 moveInput;
+  Rigidbody2D myRigidbody2D;
+  Animator myAnimator;
+  CapsuleCollider2D myCapsuleCollider2D;
+  float gravityScaleAtStart;
+
+  void Start()
+  {
+    myRigidbody2D = GetComponent<Rigidbody2D>();
+    myAnimator = GetComponent<Animator>();
+    myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+    gravityScaleAtStart = myRigidbody2D.gravityScale;
+  }
+
+  void Update()
+  {
+    Run();
+    FlipSprite();
+    ClimbingLadder();
+  }
+
+  void OnMove(InputValue value)
+  {
+    moveInput = value.Get<Vector2>();
+  }
+
+  void OnJump(InputValue value)
+  {
+    if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+    {
+      return;
+    }
+
+    if (value.isPressed)
+    {
+      // do something
+      myRigidbody2D.velocity += new Vector2(0f, jumpSpeed);
+    }
+  }
+
+  void Run()
+  {
+    Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody2D.velocity.y);
+    myRigidbody2D.velocity = playerVelocity;
+
+    bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+    // recebo minha animacao de correndo p true 
+    myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+  }
+
+  void FlipSprite()
+  {
+    bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+    if (playerHasHorizontalSpeed)
+    {
+      transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+    }
+  }
+
+  void ClimbingLadder()
+  {
+    if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+    {
+      myRigidbody2D.gravityScale = gravityScaleAtStart;
+      return;
+    }
+
+    Vector2 climbVelocity = new Vector2(myRigidbody2D.velocity.x, moveInput.y * climbSpeed);
+    myRigidbody2D.velocity = climbVelocity;
+    myRigidbody2D.gravityScale = 0f;
+
+    bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody2D.velocity.y) > Mathf.Epsilon;
+    myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    Debug.Log("Just a log");
+  }
+}
